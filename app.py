66b88ca60fdb984555efa6bea8ead351e56bc543
KirillKor21 <pro100kirik921@mail.ru>
2023-01-10 19:48:40 +0300

    Docker-compose finaly

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ def about():
     return render_template("about.html")
 
 
+@app.route("/about/kirill")
+def kirill():
+    return "Kirill how are you,"
+
+
 @app.route("/user/<string:name>/<int:id>")
 def user(name, id):
     return "User is " + name + " id is " + str(id)
